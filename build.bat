051da9eb618cd03aa6c94b99c7b8697dc02ac17c
Michael Chourdakis <windowssnt@gmail.com>
2024-08-11 08:02:50 +0300

    Signed Executables

--- build.bat
+++ build.bat
@@ -2,4 +2,5 @@
 
 msbuild ecdc_mft.sln /clp:ErrorsOnly /m /p:Configuration="Release" /p:Platform=x64
 msbuild ecdc_mft.sln /clp:ErrorsOnly /m /p:Configuration="Release" /p:Platform=x86
- 
+copy .\x64\Release\ecdc_mft_installer.exe ecdc_mft_installer64.exe /y
+copy .\Release\ecdc_mft_installer.exe ecdc_mft_installer32.exe /y
